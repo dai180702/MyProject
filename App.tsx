2de3b744049c7ec9a00c974a6e1d97f8465c0b8b
da180702 <phanminhdai.it@gmail.com>
2025-04-01 14:04:07 +0700

    test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  //jsx
   return (
     <View style={styles.container}>
+      <View></View>
+
+      <Text style={styles.hello1}> Hello </Text>
+      <Text> Xin chao 1</Text>
+      <Text style={styles.child}>
+        <Text style={styles.child1}>Xin chao 2</Text>
+        Xin chao 3
+      </Text>
       <Text>Open up App.tsx to start </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -16,5 +23,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  hello1: {
+    color: "red",
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    fontWeight: "bold",
+  },
+
+  child: {
+    color: "pink",
+    fontSize: 50,
+  },
+
+  child1: {
+    color: "blue",
   },
 });
